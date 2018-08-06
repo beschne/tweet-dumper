b2e916e9f8dc101ba49107d2131fb22b8734f1f4
Benno Schneider <Benno.Schneider@gmail.com>
2018-08-06 17:59:28 +0200

    Tweet type, user name, and screen name added to Excel file.

--- export-xls.rb
+++ export-xls.rb
@@ -29,30 +29,35 @@ row[1] = 'created'
 row[2] = 'tweet'
 row[3] = 'rt'
 row[4] = 'fav'
-row[5] = 'lang'
-row[6] = 'client'
+row[5] = 'tweet'
+row[6] = 'user'
+row[7] = 'screen name'
+row[8] = 'lang'
+row[9] = 'client'
 row.set_format 1, format_date
 row.set_format 3, format_center
 row.set_format 4, format_center
 row.set_format 5, format_center
 
 tweets.each.with_index(1) do |tweet, i|
-  puts "#{i} #{tweet.id.to_s}"
+  puts "#{i} #{tweet.attrs[:id_str]}"
   row = sheet.row(i)
   sheet.row(i).default_format = format
   # Excel functions are not yet supported by Spreadsheet gem
   # row[0] = "=HYPERLINK(\"#{tweet.uri}\"; \"#{tweet.id.to_s}\")"
-  row[0] = tweet.id.to_s
+  row[0] = tweet.attrs[:id_str]
   row[1] = tweet.created_at
   # current v6.2.0 Twitter gem does not support full_text correctly
   # https://github.com/sferik/twitter/issues/880
-  full_text = tweet.retweeted? ? tweet.attrs[:retweeted_status][:full_text]
-                               : tweet.attrs[:full_text]
-  row[2] = full_text
+  row[2] = tweet.attrs[:full_text]
   row[3] = tweet.retweet_count
   row[4] = tweet.favorite_count
-  row[5] = tweet.lang
-  row[6] = Sanitize.clean(tweet.source)
+  row[5] = !tweet.retweeted_status.nil? ? 'retweet'
+           : (tweet.in_reply_to_status_id.nil? ? 'tweet' : 'reply')
+  row[6] = tweet.attrs[:user][:name]
+  row[7] = tweet.attrs[:user][:screen_name]
+  row[8] = tweet.lang
+  row[9] = Sanitize.clean(tweet.source)
   row.set_format 1, format_date
   row.set_format 3, format_center
   row.set_format 4, format_center
